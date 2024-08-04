197c72c1cc5994b20cb8141d1f89d06fdff247cf
unknown <r.kevensantos7@gmail.com>
2024-08-04 02:10:31 -0300

    função de editar e bug de restate de usuario resolvido

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const porta = 3000;
 
-const UserFile = path.join('./users')
+let userG;
 
 const app = express();
 
-app.engine('handlebars', exphbs.engine())
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'));
 
 app.use(
     express.urlencoded({
@@ -20,26 +26,29 @@ app.use(express.json())
 
 app.get('/user', (req, res) => {
 
-    console.log(req.params)
-
-    // const nome = w
-    // const idade = w
-    // const biografia = w
-    // const hobbies = sd
-
-    res.render('user', {})
+    res.render('user')
 
 })
 
 app.post('/acess/user', (req, res) => {
 
     const user = req.body.user
+    userG = user
 
      if(!ChecKAccounts(user)){
         return res.render('home')
     }
 
+    DefinirConta(user)
+
     const AccountData = PegarConta(user)
+
+    if(!fs.existsSync(`./views/users/check.json`)){
+        fs.writeFileSync('./views/users/check.json',`{"check": "${user}"}`)
+    }else{
+        fs.writeFileSync('./views/users/check.json',`{"check": "${user}"}`)
+    }
+
 
     const valores = {
         nome: AccountData.nome,
@@ -51,28 +60,85 @@ app.post('/acess/user', (req, res) => {
     res.render(`user`, {valores})
 })
 
+app.get('/perfil/editar', (req, res) => {
+
+    const user = PegarConta('check')
+
+    const AccountData = PegarConta(user.check)
+
+    const valores = {
+        nome: AccountData.nome,
+        idade: AccountData.idade,
+        biografia: AccountData.biografia,
+        hobbies: AccountData.hobbies,
+        user: AccountData.userbase
+    }
+
+    // console.log(valores)
+
+    res.render('editarPerfil', {valores})
+
+})
+
+app.post('/perfil/editado', (req, res) => {
+
+    const user = req.body.user
+
+    const AccountData = PegarConta(user)
+
+     AccountData.nome = req.body.nome;
+     AccountData.idade = req.body.idade;
+     AccountData.biografia = req.body.biografia;
+     AccountData.hobbies = req.body.hobbies;
+
+     const valores = {
+        nome: AccountData.nome,
+        idade: AccountData.idade,
+        biografia: AccountData.biografia,
+        hobbies: AccountData.hobbies,
+        user: AccountData.userbase
+    }
+
+    fs.writeFileSync(`./views/users/${valores.user}.json`,
+        JSON.stringify(AccountData),
+        function(err){console.log(err)}
+    )
+
+    res.render('user', {valores})
+})
+
 app.get('/', (req, res) => {
 
     res.render('home')
 
 })
 
-app.listen(porta, () => {
-    console.log(`servidor rodando na porta ${porta}`)
-})
+function DefinirConta(conta){
+    console.log('pass')
+    const user = PegarConta(conta)
+    user.userbase = conta
+    fs.writeFileSync(`./views/users/${conta}.json`, 
+        JSON.stringify(user),
+        (err)=>{console.log(err)}
+    )
+    console.log(user)
+     console.log('user definido com sucesso')
+}
 
 function ChecKAccounts(NomeConta){
-    console.log(NomeConta)
     if(!fs.existsSync(`./views/users/${NomeConta}.json`)){
         console.log('A conta não existe')
         return false
-    }else{
-        console.log('conta encontrada com sucesso!!')
-        return true
     }
+    return true
+
 }
 
 function PegarConta(NomeConta){
     const NomeJson = fs.readFileSync(`./views/users/${NomeConta}.json`)
     return JSON.parse(NomeJson)
 }
+
+app.listen(porta, () => {
+    console.log(`servidor rodando na porta ${porta}`)
+})
